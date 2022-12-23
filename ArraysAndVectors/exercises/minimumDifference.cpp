47f05e06d704272188f5377d1bcf6ca82c013c2c
Ryan S <ryansateles@gmail.com>
2022-12-23 20:49:16 -0300

    MinimumDifference solution

--- ArraysAndVectors/exercises/minimumDifference.cpp
+++ ArraysAndVectors/exercises/minimumDifference.cpp
@@ -36,6 +36,46 @@ pair<int, int> minDifference(vector<int> a, vector<int> b)
     return minPair;
 }
 
+// O(n log n + m log m) - time
+// O(1) - space
+pair<int,int> minDifference2(vector<int> a,vector<int> b){
+    //Complete this method
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    
+    int aSize = a.size();
+    int bSize = b.size();
+    
+    int pointerA = 0;
+    int pointerB = 0;
+    int minDiff = abs(a[0] - b[0]);
+    pair<int,int> ans(a[0],b[0]);
+    while(pointerA != (aSize-1) and pointerB != (bSize-1)){
+        int numA = a[pointerA];
+        int numB = b[pointerB];
+        
+        int diff = abs(numA - numB);
+        
+        if (diff == 0){
+            return make_pair(numA,numB);
+        }
+        if(diff < minDiff){
+            ans = make_pair(numA,numB);
+            minDiff = diff;
+        }
+        
+        if(pointerA < aSize and numA < numB){
+            pointerA++;
+        }else if (pointerB < bSize and numA > numB){
+            pointerB++;
+        }
+    }
+    return ans;
+    
+}
+
+
+
 int main()
 {
     auto sla = minDifference({23, 5, 10, 17, 30}, {26, 134, 135, 14, 19});
