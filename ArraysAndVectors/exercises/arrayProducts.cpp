8ddee83ad5ca6fb5830436520ff33ed10baa5a0b
Ryan S <ryansateles@gmail.com>
2022-12-24 11:35:23 -0300

    ArrayProducts - not finished

--- ArraysAndVectors/exercises/arrayProducts.cpp
+++ ArraysAndVectors/exercises/arrayProducts.cpp
@@ -17,6 +17,67 @@ vector<int> productArray(vector<int> arr)
     return output;
 }
 
-int main()
-{
+//Bad Solution - Using Divison
+//O(n) - time
+//O(1) - space (excluding output)
+vector<int> productExceptSelf(vector<int> nums) {
+    int n = nums.size();
+    
+    vector<int> output(n,0);
+
+    int product = 1;
+    int hasZero = 0;
+    for(int num : nums){
+        if (num){
+            product *= num;
+        }else{
+            hasZero++;
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        if (nums[i] == 0 and hasZero == 1){
+            output[i] = product;
+        } else if (hasZero){
+            output[i] = 0;
+        } else {
+            output[i] = product / nums[i];
+        }
+    }
+
+    return output;
+}
+
+//Great Solution - Without Division
+//O(n) - time
+//O(1) - space (excluding output)
+//TODO - finish this exercise
+vector<int> productExceptSelf2(vector<int> nums) {
+    int n = nums.size();
+    
+    vector<int> output(n,1);
+
+    return output;
+}
+
+int main(){
+    //Some Test Cases
+    vector<int> arr1 = {1,2,3,4}; //[24,12,8,6]
+    vector<int> arr2 = {1,2,3,4,5}; //[120,60,40,30,24]
+    vector<int> arr3 = {-1,1,0,-3,3}; //[0,0,9,0,0]
+    vector<int> arr4 = {0,0,0,0,0}; //[0,0,0,0,0]
+    vector<int> arr5 = {1,0,3,0,0}; //[0,0,0,0,0]
+
+    auto testCases = {arr1, arr2, arr3, arr4, arr5};
+
+    // pointer to function
+    vector<int> (*solve)(vector<int>);
+    solve = &productExceptSelf; // Change this to the function you want to test
+
+    for(auto arr : testCases){
+        auto output = (*solve)(arr);
+        for(auto e : output)
+            cout << e << " ";
+        cout << endl;
+    }
 }
