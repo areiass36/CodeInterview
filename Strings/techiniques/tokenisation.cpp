6da1aa821c96d0bc0a3e6d9553ae82b912f7e991
Ryan S <ryansateles@gmail.com>
2022-12-26 21:34:43 -0300

    tokenisation - myStrtok version

--- Strings/techiniques/tokenisation.cpp
+++ Strings/techiniques/tokenisation.cpp
@@ -39,13 +39,50 @@ void tokenisationStrtok2(char *str){
     cout << endl;
 }
 
+char *mystrtok(char *str, char delim){
+    static char *input = NULL;
+
+    if (str != NULL)
+        input = str;
+
+    if (input == NULL)
+        return NULL;
+    
+    char *token = new char[strlen(input)+1];
+    int i = 0;
+    for( ; input[i] != '\0'; i++){
+        if (input[i] == delim){
+            token[i] = '\0';
+            input += i + 1;
+            return token;
+        }else {
+            token[i] = input[i];
+        }
+    }
+    token[i] = '\0';
+    input = NULL;
+
+    return token;
+}
+
 int main() {
     
     /* Using std::string */
-    method1();
+    //method1();
 
     /* Using char pointer */
     //method2();
+
+    //Designed strtok 
+    char str[1000];
+    cin.getline(str,1000);
+
+    char *token = mystrtok(str, ' ');
+
+    while(token != NULL){
+        cout << token << endl;
+        token = mystrtok(NULL, ' ');
+    }
 
     return 0;
 }
