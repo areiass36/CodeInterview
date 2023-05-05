d3d661ac15ee7482ec15de391ae3abfb4274a297
Ryan S <ryansouza@usp.br>
2023-05-05 12:52:15 -0300

    Smart Keypad - code (course solution added)

--- Recursion/techniques/phoneKeypad.cpp
+++ Recursion/techniques/phoneKeypad.cpp
@@ -2,7 +2,9 @@
 
 using namespace std;
 
-unordered_map<int,string> keypad = {
+//Ryan Solution
+unordered_map<int,string> keypad2 = {
+    {0,""},
     {1,""},
     {2,"ABC"},
     {3,"DEF"},
@@ -10,8 +12,8 @@ unordered_map<int,string> keypad = {
     {5,"JKL"},
     {6,"MNO"},
     {7,"PQRS"},
-    {6,"TUV"},
-    {6,"WXYZ"}
+    {8,"TUV"},
+    {9,"WXYZ"}
 };
 
 //O(4^n) - Time (Where "n" is the length of num input)
@@ -24,7 +26,12 @@ void generateCombination(string num, string comb, vector<string> &ans){
     
     int currentDigit = num[0] - '0';
 
-    for(char ch : keypad[currentDigit]){
+    //skip 0 and 1
+    if (currentDigit == 0 or currentDigit == 1){
+        generateCombination(num.substr(1), comb, ans);
+    }
+
+    for(char ch : keypad2[currentDigit]){
         generateCombination(num.substr(1), comb + ch, ans);
         
         //it's not necessary the exclude case because the exercise only want the **combinations** and not subsets 
@@ -32,14 +39,38 @@ void generateCombination(string num, string comb, vector<string> &ans){
     }
 }
 
+//Course Solution
+string keypad[] = {"","","ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"};
+
+void printKeypadOutput(string input, string output, int i = 0){
+    if (input[i] == '\0'){
+        cout << output << ',';
+        return;
+    }
+
+    //rec case
+    // if the input is 0 or 1 them we skip it
+    int currentDigit = input[i] - '0';
+    if (currentDigit == 0 or currentDigit == 1){
+        printKeypadOutput(input, output, i + 1);
+    }
+
+    //keypad
+    for(int k = 0; k < keypad[currentDigit].size(); k++){
+        printKeypadOutput(input, output + keypad[currentDigit][k], i + 1);
+    }
+}
+
 int main() {
     vector<string> ans;
-    generateCombination(to_string(23), "", ans);
-
+    generateCombination(to_string(351), "", ans);
+    
     for(string comb : ans){
         cout << comb << ",";
     }
     cout << endl;
+
+    printKeypadOutput("351", "");
 
     return 0;
 }
